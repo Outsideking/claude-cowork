6f47e3462a8fd5340dcb5e8b3592ad46029b04e5
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-03-04 20:44:56 +0700

    Update proscService.js

--- src/services/proscService.js
+++ src/services/proscService.js
@@ -1,1 +1,9 @@
+import axios from "axios";
 
+export async function callProsc(data) {
+  return axios.post("http://localhost:3000/dynamic/analyzeUser/v1", data, {
+    headers: {
+      Authorization: Buffer.from("admin:supersecret").toString("base64")
+    }
+  });
+}
